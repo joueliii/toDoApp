98a7fa210674c4bf021d6330c74a9b619a7f8826
Saku Junni <junnisaku@gmail.com>
2017-10-13 20:34:47 +0300

    Main pages added
    added tabs, menu

---
+++ src/pages/tab1/tab1.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service/auth-service';
+/**
+ * Generated class for the Tab1Page page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-tab1',
+  templateUrl: 'tab1.html',
+})
+export class Tab1Page {
+  username = '';
+  email = '';
+  constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthService) {
+    let info = this.auth.getUserInfo();
+    this.username = info['name'];
+    this.email = info['email'];
+  }
+
+  public logout() {
+    this.auth.logout().subscribe(succ => {
+      this.navCtrl.setRoot('LoginPage')
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Tab1Page');
+  }
+
+}
