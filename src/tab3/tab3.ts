fdeef157494ab9d157ea663c9752f0ff70facabe
joueliii <joelkoskelainen@gmail.com>
2017-10-22 18:24:10 +0300

    Logout button

--- src/tab3/tab3.ts
+++ src/tab3/tab3.ts
@@ -14,12 +14,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tab3.html',
 })
 export class Tab3Page {
+  afAuth: any;
+  nav: any;
+  auth: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Tab3Page');
+  }
+
+  public logout() {
+    this.afAuth.logout().subscribe(succ => {
+      this.nav.setRoot('LoginPage')
+    });
   }
 
 }
